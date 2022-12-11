dbeb208ae0f4a54da99e34155a33dbe6eea5b2f9
man1fest000 <lubuskijurij@gmail.com>
2022-12-11 17:55:34 +0200

    hw6-mailer-hbs-method-2

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,15 +1,18 @@
 const oauthService = require("../service/oauth.service");
 const emailService = require("../service/email.service");
+const ActionToken = require("../dataBase/ActionToken");
 const OAuth = require("../dataBase/OAuth");
-const { WELCOME } = require("../config/email-action.enum");
+const User = require("../dataBase/User");
+const { WELCOME, FORGOT_PASS } = require("../config/email-action.enum");
+const { FORGOT_PASSWORD } = require("../config/token-action.enum");
+const { FRONTEND_URL } = require("../config/config");
 
 module.exports = {
   login: async (req, res, next) => {
     try {
       const { user, body } = req;
 
-      console.log(WELCOME, '- AUTH CONTROLLER');
-      await emailService.sendEmail('victor.fzs10@gmail.com', WELCOME, { userName: user.name });
+      await emailService.sendEmail(user.email, WELCOME, { userName: user.name, array: [{ number: 1}, { number: 2}, { number: 3}], condition: false });
 
       await oauthService.comparePasswords(user.password, body.password);
 
@@ -37,6 +40,37 @@ module.exports = {
       await OAuth.create({ ...tokenPair, _user_id })
 
       res.status(201).json(tokenPair);
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  forgotPassword: async (req, res, next) => {
+    try {
+      const { _id, email, name } = req.user;
+
+      const actionToken = oauthService.generateActionToken(FORGOT_PASSWORD, { email: email });
+      const forgotPassFEUrl = `${FRONTEND_URL}/password/new?token=${actionToken}`;
+
+      await ActionToken.create({ token: actionToken, tokenType: FORGOT_PASSWORD, _user_id: _id });
+      await emailService.sendEmail(email, FORGOT_PASS, { url: forgotPassFEUrl, userName: name });
+
+      res.json('ok');
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  forgotPasswordAfterForgot: async (req, res, next) => {
+    try {
+      const { user, body } = req;
+
+      const hashPassword = await oauthService.hashPassword(body.password);
+
+      await ActionToken.deleteOne({ token: req.get('Authorization') });
+      await User.updateOne({ _id: user._id }, { password: hashPassword });
+
+      res.json('ok');
     } catch (e) {
       next(e);
     }
